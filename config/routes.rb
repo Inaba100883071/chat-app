551fc9db12c4799f3da25b33c45be5151a3f7ed2
Inaba100883071 <tony.mark85blog@gmail.com>
2020-11-05 13:01:52 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   get 'messages/index'
+=======
+  devise_for :users
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
 end

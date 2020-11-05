8fbe7262e5d3f62a943a788e5d0422dc4c26f682
Inaba100883071 <tony.mark85blog@gmail.com>
2020-11-05 16:31:33 +0900

    ユーザー認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  get 'messages/index'
-=======
   devise_for :users
->>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
 end
